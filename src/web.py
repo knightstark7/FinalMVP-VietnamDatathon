5aba544cd4c51682cbd4547e90143d533ab3eb46
ImPhatDat <phatdatn2003@gmail.com>
2023-12-16 10:15:41 +0700

    upload new data

---
+++ src/web.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="Final MVP Datada",
+    page_icon="✅",
+    layout="wide",
+)
